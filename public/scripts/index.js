f20f85b2f2e228e5c68c4a98bab8cfd4723a3ad6
wov <wovfeng@gmail.com>
2014-04-30 17:06:03 +0800

    finish front

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,29 @@
 $(function(){
-    
-    
+
+    var _input = document.getElementById('upload');
+
+    _input.addEventListener('change',function(){
+        var files = _input.files;
+
+        var formdata = new FormData(document.getElementById('form'));
+
+        var xhr = new XMLHttpRequest();
+        var upload = xhr.upload;
+
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState === 4){
+                $('.popout').show();
+                $('.popout .content').html(xhr.responseText);
+            }
+        };
+
+        xhr.open('POST',url); //url 是表单的提交地址。
+        xhr.send(formdata);
+    });
+
+
+    $('.close').on('click',function(){
+        $(this).parent().hide();
+    });
+
 });
