63497a9fbb7c2fb2a7ce4d17bff5a59466541b76
kevin14 <kevin14@me.com>
2014-05-07 11:11:43 +0800

    add windows support

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 var exec = require('child_process').exec;
-// var unzip = require('unzip');
 
 /* GET home page. */
 router.get('/', function(req, res) {
+
     res.render('index', {
         title: 'Express'
     });
@@ -16,25 +16,41 @@ router.post('/upload', function(req, res) {
     if (req.files.file) {
         if (req.files.file.extension == "zip") {
 
-            exec("unzip "+req.files.file.path+" -d public/prototypes/"+path.basename(req.files.file.name,'.zip') + " && rm -rf public/prototypes/"+path.basename(req.files.file.name,'.zip')+"/__MACOSX*",function(err,stdout, stderr){
-                if (err) {
-                    throw err;
-                }else{
+            if (req.headers['user-agent'].indexOf("windows") > 0) {
+                exec("mkdir public/prototypes/" + path.basename(req.files.file.name, '.zip') + " && python uploads/myzip -x -d public/prototypes/" + path.basename(req.files.file.name, '.zip') + " uploads/" + req.files.file.name + " && rm -rf public/prototypes/" + path.basename(req.files.file.name, '.zip') + "/__MACOSX*", function(err, stdout, stderr) {
+                    if (err) {
+                        throw err;
+                    } else {
+                        exec("rm " + req.files.file.path);
+                        fs.readdir(__dirname + '/../public/prototypes/' + path.basename(req.files.file.name, '.zip'), function(err, list) {
+                            console.log(list);
+                            res.send({
+                                res_code: 1,
+                                path: '/prototypes/' + path.basename(req.files.file.name, '.zip'),
+                                list: list
+                            });
+                        })
+                    }
+                });
+            } else {
+                exec("unzip " + req.files.file.path + " -d public/prototypes/" + path.basename(req.files.file.name, '.zip') + " && rm -rf public/prototypes/" + path.basename(req.files.file.name, '.zip') + "/__MACOSX*", function(err, stdout, stderr) {
+                    if (err) {
+                        throw err;
+                    } else {
+                        exec("rm " + req.files.file.path);
+                        fs.readdir(__dirname + '/../public/prototypes/' + path.basename(req.files.file.name, '.zip'), function(err, list) {
+                            console.log(list);
+                            res.send({
+                                res_code: 1,
+                                path: '/prototypes/' + path.basename(req.files.file.name, '.zip'),
+                                list: list
+                            });
+                        })
+                    }
+                })
+            }
 
-                    console.log(__dirname +  '/../public/prototypes/'+path.basename(req.files.file.name,'.zip'));
 
-                    fs.readdir(__dirname +  '/../public/prototypes/'+path.basename(req.files.file.name,'.zip'),function(err,list){
-                        // console.log(list);
-                         res.send({
-                            res_code:1,
-                            path:'/prototypes/'+path.basename(req.files.file.name,'.zip'),
-                            list : list
-                        });
-                    })
-               
-                    exec("rm -rf "+req.files.file.path);
-                }
-           })
         } else {
             res.send("上传文件格式不正确！");
         }
@@ -45,21 +61,50 @@ router.post('/upload', function(req, res) {
 
 router.get('/list', function(req, res) {
 
-    var realPath = path.join(__dirname,'../public/prototypes/');
+    var realPath = path.join(__dirname, '../public/prototypes/');
 
-    fs.readdir(realPath, function(err,files){
+    fs.readdir(realPath, function(err, files) {
         if (err) {
             res.send({
-                res_code:0
+                res_code: 0
             })
             throw err;
-        }else{
+        } else {
             res.send({
-                res_code:1,
-                list:files
+                res_code: 1,
+                list: files
             })
         }
     })
 })
+
+
+//解压
+
+function getGzipped(url, callback) {
+    // buffer to store the streamed decompression
+    var buffer = [];
+    console.log(url)
+    http.get(url, function(res) {
+        console.log(res)
+        // pipe the response into the gunzip to decompress
+        var gunzip = zlib.createUnzip();
+        res.pipe(gunzip);
+
+        gunzip.on('data', function(data) {
+            // decompression chunk ready, add it to the buffer
+            buffer.push(data.toString())
+
+        }).on("end", function() {
+            // response and decompression complete, join the buffer and return
+            callback(null, buffer.join(""));
+
+        }).on("error", function(e) {
+            callback(e);
+        })
+    }).on('error', function(e) {
+        callback(e)
+    });
+}
 
 module.exports = router;
